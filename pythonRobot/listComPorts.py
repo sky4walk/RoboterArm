192e224a10438d53aa407cf6d576bf43ad9078af
Betz <andre.betz@siemens-healthineers.com>
2018-12-03 14:29:19 +0100

    added avrdude

--- pythonRobot/listComPorts.py
+++ pythonRobot/listComPorts.py
@@ -8,10 +8,7 @@ import time
 import serial
 import serial.tools.list_ports as port_list
 
-port = "COM10"
-baud = 9600
+ports = list(port_list.comports())
+for p in ports:
+    print (p)
 
-ser = serial.Serial(port, baud, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE )
-time.sleep(2)
-startStr = ser.readline()
-print(startStr)
